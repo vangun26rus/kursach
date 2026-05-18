8f464d9f6945134453ba50f6762a0f0b0260ad4f
vangun26rus <vangun26@gmail.com>
2026-05-18 19:05:13 +0300

    ошибка какая-то

--- backend/DigitalHelpSystem.Api/obj/Debug/net9.0/DigitalHelpSystem.Api.AssemblyInfo.cs
+++ backend/DigitalHelpSystem.Api/obj/Debug/net9.0/DigitalHelpSystem.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalHelpSystem.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff67572371aebbb38def9055cece9a6f1b20e8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e1f1d4b49f8a86a4a88fbe9323126643e98f65")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalHelpSystem.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalHelpSystem.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
